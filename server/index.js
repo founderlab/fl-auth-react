d69e12bf8268a4b39eeff0585e653cd3172fa242
gwilym <gwilym@gwilymh.com>
2015-10-06 14:15:23 +1100

    logging

--- server/index.js
+++ server/index.js
@@ -32,7 +32,6 @@ export default function configure(options={}) {
   if (!options.app) throw new Error('fl-auth-server init: Missing app from options')
   options.User = options.User || options.user_model_type || require('./models/user')
 
-  console.log('auth options:', options)
   configureMiddleware(options)
   configureSerializing(options)
   configureStrategies(options)
